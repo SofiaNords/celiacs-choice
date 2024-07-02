47ba10bc24c9fec7bce03953d96af080b16bbf19
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-07-02 12:22:29 +0000

    Create NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import styles from '../styles/NavBar.module.css';
+
+const NavBar = () => {
+    return (
+        <Navbar className={styles.NavBar} expand="lg" fixed="top">
+            <Container>
+                <Navbar.Brand href="#home">Celiac's Choice</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto text-left">
+                        <Nav.Link><i className='fas fa-home'></i>Home</Nav.Link>
+                        <Nav.Link><i className='fa-solid fa-circle-info'></i>About</Nav.Link>
+                        <Nav.Link><i className='fas fa-sign-in alt'></i>Sign in</Nav.Link>
+                        <Nav.Link><i className='fas fa-user-plus'></i>Sign up</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default NavBar
