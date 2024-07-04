b37e767720388e774cf56490b0ad087681dbc43d
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-07-04 06:12:05 +0000

    Add Selected posts and Sign out to Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,55 @@ import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import styles from '../styles/NavBar.module.css';
 import { NavLink } from 'react-router-dom';
-import { useCurrentUser } from '../contexts/CurrentUserContext';
+import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
+import Avatar from './Avatar';
+import axios from 'axios';
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
-    const loggedInIcons = <>{currentUser?.username}</>
+    const setCurrentUser = useSetCurrentUser();
+
+    const handleSignOut = async () => {
+        try {
+          await axios.post("dj-rest-auth/logout/");
+          setCurrentUser(null);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
+    const addPostIcon = (
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/posts/create"
+        >
+            <i className='far fa-plus-square'></i>Add post
+        </NavLink>
+    )
+    const loggedInIcons =
+        <>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to='/selected'
+            >
+                <i className='fa-solid fa-square-check'></i>Selected posts
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                to='/'
+                onClick={handleSignOut}
+            >
+                <i className='fas fa-sign-out-alt'></i>Sign out
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                to={`/profiles/${currentUser?.profile_id}`}
+            >
+                <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+            </NavLink>
+        </>
     const loggedOutIcons = <>
         <NavLink
             className={styles.NavLink}
@@ -25,7 +69,11 @@ const NavBar = () => {
         <Navbar className={styles.NavBar} expand="lg" fixed="top">
             <Container>
                 <NavLink to="/">
-                    <Navbar.Brand href="#home">Celiac's Choice</Navbar.Brand></NavLink>
+                    <Navbar.Brand href="#home">
+                        Celiac's Choice
+                    </Navbar.Brand>
+                </NavLink>
+                {currentUser && addPostIcon}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
