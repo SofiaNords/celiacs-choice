13e7c55ff1070d77632ca6a1bff8565b1cdf3408
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-07-06 07:12:10 +0000

    Create SelectedPosts component

---
+++ src/pages/selected/SelectedPosts.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
+import appStyles from "../../App.module.css";
+import Asset from "../../components/Asset";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
+const SelectedPosts = () => {
+    const [postData, setPostData] = useState({
+        // we will use the pagePost later!
+        pagePost: { results: [] },
+        selectedPosts: { results: [] },
+    });
+    const { selectedPosts } = postData;
+    const currentUser = useCurrentUser();
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const { data } = await axiosReq.get(
+                    "/posts/?ordering=-select_count"
+                );
+                setPostData((prevState) => ({
+                    ...prevState,
+                    selectedPosts: data,
+                }));
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        handleMount();
+    }, [currentUser]);
+
+    return (
+        <Container className={appStyles.Content}>
+            {selectedPosts.results.length ? (
+                <>
+                <p>Selected Choices List</p>
+            {selectedPosts.results.map((post) => (
+                <p key={post.id}>{post.title}</p>
+            ))}
+        </>
+    ) : (
+        <Asset spinner />
+    )
+}
+        </Container >
+    );
+};
+
+export default SelectedPosts;
